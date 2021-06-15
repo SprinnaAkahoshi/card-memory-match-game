0d10c65c148cb831132aea6d317f54d0f78025bd
SprinnaAkahoshi <sprinnaa@zillowgroup.com>
2021-06-14 22:27:22 -0700

    Updated to 2 instances of card randomizer

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const cards = document.querySelectorAll(".card");
-const fruitArray = ["citrus", "kiwi", "orange", "blueberries"];
+const fruitArray = [
+  "citrus",
+  "kiwi",
+  "orange",
+  "blueberries",
+  "citrus",
+  "kiwi",
+  "orange",
+  "blueberries",
+];
 
 const randomizer = () => Math.floor(Math.random() * fruitArray.length);
 
 for (let i = 0; i < cards.length; i++) {
-  const randomFruit = fruitArray[randomizer()];
+  const randomNum = randomizer();
+  const randomFruit = fruitArray.splice(randomNum, 1)[0];
   cards[i].onclick = () => {
     cards[i].classList.add(randomFruit);
   };
