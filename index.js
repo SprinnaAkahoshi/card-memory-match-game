8d9aacd6f752639151427b55e8b365919250b882
SprinnaAkahoshi <sprinnaa@zillowgroup.com>
2021-06-15 22:50:43 -0700

    added card-cover and on click functionality

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll("[data-card]");
 const fruitArray = [
   "citrus",
   "kiwi",
@@ -15,7 +15,8 @@ const randomizer = () => Math.floor(Math.random() * fruitArray.length);
 for (let i = 0; i < cards.length; i++) {
   const randomNum = randomizer();
   const randomFruit = fruitArray.splice(randomNum, 1)[0];
+  cards[i].classList.add(randomFruit);
   cards[i].onclick = () => {
-    cards[i].classList.add(randomFruit);
+    cards[i].children[0].classList.add("hide");
   };
 }
