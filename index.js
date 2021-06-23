d25bdd8a75323516c551b590e29d9614c800b131
SprinnaAkahoshi <sprinnaa@zillowgroup.com>
2021-06-22 22:42:38 -0700

    added matching logic

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const cards = document.querySelectorAll("[data-card]");
+let firstClick = null;
+let secondClick = null;
 const fruitArray = [
   "citrus",
   "kiwi",
@@ -17,6 +19,26 @@ for (let i = 0; i < cards.length; i++) {
   const randomFruit = fruitArray.splice(randomNum, 1)[0];
   cards[i].classList.add(randomFruit);
   cards[i].onclick = () => {
-    cards[i].children[0].classList.add("hide");
+    const cardCover = cards[i].children[0];
+    cardCover.classList.add("hide");
+    cards[i].classList.add("no-click");
+    if (!firstClick && !secondClick) {
+      firstClick = cards[i];
+    } else if (firstClick && !secondClick) {
+      secondClick = cards[i];
+      const firstClickClasses = firstClick.classList.value;
+      const secondClickClasses = secondClick.classList.value;
+      if (firstClickClasses !== secondClickClasses) {
+        firstClick.children[0].classList.remove("hide");
+        secondClick.children[0].classList.remove("hide");
+        firstClick.classList.remove("no-click");
+        secondClick.classList.remove("no-click");
+      } else if (firstClickClasses === secondClickClasses) {
+        // firstClick.classList.add("no-click");
+        // secondClick.classList.add("no-click");
+      }
+      firstClick = null;
+      secondClick = null;
+    }
   };
 }
